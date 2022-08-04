d5ec817ef757e3ba77c5d319e73cc0d34ac69218
fak1ch <slyvayva@mail.ru>
2022-08-04 10:29:09 +0300

    AS_LevelSpawner: Added LevelSpawner

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class Utils
+{
+    public static bool CheckRandomless(int procent)
+    {
+        if (procent == 0)
+            return false;
+
+        return Random.Range(0, 100 / procent) == 0;
+    }
+
+    public static float GetProcent(float a, float b, float value)
+    {
+        if (b - a == 0)
+            return 0;
+
+        return (value - a) / (b - a);
+    }
+}
