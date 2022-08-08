f72c887b5a8a860c42a5480288bc30b3ba6a8d53
fak1ch <slyvayva@mail.ru>
2022-08-08 12:22:46 +0300

    AS_Ball-Movement: Added few things
    1) Upgrade architecture, added onTriggerEnterEvent ans so on.
    2) Mechanics change ball angle when he contact with an object

--- Assets/App/Scripts/Architecture/CustomBehaviour.cs
+++ Assets/App/Scripts/Architecture/CustomBehaviour.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Architecture
 {
     public abstract class CustomBehaviour
     {
         public virtual void Initialize() { }
+
         public virtual void Tick() { }
         public virtual void FixedTick() { }
+
+        public virtual void CollisionEnter2D(Collision2D collision) { }
+        public virtual void CollisionExit2D(Collision2D collision) { }
+        public virtual void TriggerEnter2D(Collider2D collider) { }
+        public virtual void TriggerExit2D(Collider2D collider) { }
+
         public virtual void Dispose() { }
     }
 }
---
+++ Assets/App/Scripts/General/ColliderEvents/TriggerExit2DEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace ColliderEvents
+{
+    public class TriggerExit2DEvent : MonoBehaviour
+    {
+        public Collider2D Collider { get; private set; }
+
+        private void OnTriggerExit2D(Collider2D collider)
+        {
+            if (collider != null)
+            {
+                Collider = collider;
+            }
+            else
+            {
+                Collider = null;
+            }
+        }
+    }
+}
+
